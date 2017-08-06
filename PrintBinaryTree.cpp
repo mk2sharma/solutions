3cd00703f3cbb92f45a97739c9cb621ec19d3464
mk2sharma <mk2sharma@gmail.com>
2017-08-05 23:46:32 -0700

    Create PrintBinaryTree.cpp

---
+++ PrintBinaryTree.cpp
@@ -0,0 +1,95 @@
+/*
+655. Print Binary Tree My SubmissionsBack to Contest
+
+Print a binary tree in an m*n 2D string array following these rules:
+
+The row number m should be equal to the height of the given binary tree.
+The column number n should always be an odd number.
+The root node's value (in string format) should be put in the exactly middle of the first row it can be put. The column and the row where the root node belongs will separate the rest space into two parts (left-bottom part and right-bottom part). You should print the left subtree in the left-bottom part and print the right subtree in the right-bottom part. The left-bottom part and the right-bottom part should have the same size. Even if one subtree is none while the other is not, you don't need to print anything for the none subtree but still need to leave the space as large as that for the other subtree. However, if two subtrees are none, then you don't need to leave space for both of them.
+Each unused space should contain an empty string "".
+Print the subtrees following the same rules.
+Example 1:
+Input:
+     1
+    /
+   2
+Output:
+[["", "1", ""],
+ ["2", "", ""]]
+Example 2:
+Input:
+     1
+    / \
+   2   3
+    \
+     4
+Output:
+[["", "", "", "1", "", "", ""],
+ ["", "2", "", "", "", "3", ""],
+ ["", "", "4", "", "", "", ""]]
+Example 3:
+Input:
+      1
+     / \
+    2   5
+   / 
+  3 
+ / 
+4 
+Output:
+
+[["",  "",  "", "",  "", "", "", "1", "",  "",  "",  "",  "", "", ""]
+ ["",  "",  "", "2", "", "", "", "",  "",  "",  "",  "5", "", "", ""]
+ ["",  "3", "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]
+ ["4", "",  "", "",  "", "", "", "",  "",  "",  "",  "",  "", "", ""]]
+Note: The height of binary tree is in the range of [1, 10].
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<string>> printTree(TreeNode* root) {
+        int maxHeight = 1;
+        int w = getWidth(root, 1, maxHeight);
+        
+        vector<vector<string>> result(maxHeight, vector<string>(w));
+        
+        fillValues(root, 0, 0, w-1, result);
+        return result;
+    }
+    
+    void fillValues(TreeNode* root, int height, int start, int end, vector<vector<string>> &result) {
+        if (root == NULL)
+            return;
+        
+        int mid = (start+end)/2;
+        result[height][mid] = intToStr(root->val);
+        fillValues(root->left, height+1, start, mid-1, result);
+        fillValues(root->right, height+1, mid+1, end, result);
+    }
+    
+    int getWidth(TreeNode* root, int height, int &maxHeight) {
+        if (root == NULL)
+            return 0;
+        
+        if (height > maxHeight)
+            maxHeight = height;
+        
+        return max(getWidth(root->left, height+1, maxHeight),
+                   getWidth(root->right, height+1, maxHeight))*2 + 1;
+    }
+    
+    string intToStr(int i) {
+        ostringstream ss;
+        ss << i;
+        return ss.str();
+    }
+};
